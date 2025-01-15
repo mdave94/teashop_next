41db32f1171cd67eaf5b5c8a3936f4b786bb8ad1
molnar.david <molnar.david@wigner.hu>
2025-01-15 12:40:06 +0100

    Hero section update

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion";
 
 const Hero = () => {
-  const textVariants = {
+  const textVariants = (index: number) => ({
     initial: {
       opacity: 0,
       y: 20,
@@ -12,10 +12,10 @@ const Hero = () => {
       y: 0,
       transition: {
         duration: 1,
-        delay: 0.5,
+        delay: 0.5 + index * 0.5, // Adjust delay based on index
       },
     },
-  };
+  });
 
   const imageVariants = {
     initial: {
@@ -26,22 +26,29 @@ const Hero = () => {
       opacity: 1,
       x: 0,
       transition: {
-        duration: 2,
-        delay: 0.1,
+        duration: 3,
+        delay: 0.5,
       },
     },
   };
 
+  const heroTexts = ["TEA", "ÉLMÉNY", "KÖZÖSSÉG", "TEANDER"];
+
   return (
     <div className="flex w-screen flex-row flex-grow items-center justify-between  h-full px-[150px]">
-      <motion.div
-        className="text-6xl"
-        variants={textVariants}
-        initial="initial"
-        animate="animate"
-      >
-        TEANDER
-      </motion.div>
+      <div className="h-[400px] flex flex-col justify-center items-start  ">
+        {heroTexts.map((text, index) => (
+          <motion.div
+            key={index}
+            className="text-6xl mb-4"
+            variants={textVariants(index)}
+            initial="initial"
+            animate="animate"
+          >
+            {text}
+          </motion.div>
+        ))}
+      </div>
       <motion.div
         variants={imageVariants}
         initial="initial"
