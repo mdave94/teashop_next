9b61c5c917c2d82694a629943e3b4156ff13bb11
molnar.david <molnar.david@wigner.hu>
2025-01-17 00:00:25 +0100

    footer update

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -9,13 +9,14 @@ const Footer = () => {
 
   const footerVariatns = {
     hidden: {
+      opacity: 0,
       y: 80,
     },
     visible: {
       y: 0,
-
+      opacity: 1,
       transition: {
-        duration: 0.5,
+        duration: 0.7,
       },
     },
   };
@@ -42,66 +43,68 @@ const Footer = () => {
         variants={footerVariatns}
         initial="hidden"
         animate={isInView ? "visible" : "hidden"}
-        className="h-[200px] px-8 bg-white flex justify-between items-center "
+        className="h-[200px]  px-8 bg-white flex justify-center items-center "
       >
-        <div className="text-4xl bg-gray-300 p-2 px-4 rounded-2xl ">
-          LogoHolder
-        </div>
-        <div
-          className="flex flex-col ml-4
-        "
-        >
-          <div className="text-2xl font-bold mb-7">Címünk</div>
-          <div className="">1119 Random cím 12-14</div>
-        </div>
-
-        <div
-          className="flex flex-col ml-4
-        "
-        >
-          <div className="text-2xl font-bold mb-7">Nyitvatartás</div>
-          <div className="">Hétfőn zárva</div>
-          <div className="">K-P 12:00-19:00</div>
-          <div className="">Szombat 12:00-18:00</div>
-          <div className="">Vasárnap 13:00-17:00</div>
-        </div>
-
-        <motion.div
-          variants={footerRightSideVariants}
-          className="flex flex-col cursor-pointer ml-4 space-y-2 group"
-        >
-          <div className="relative pb-1 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-500 hover:after:w-full">
-            Felelősséválalási nyilatkozat
+        <div className="gap-14  flex justify-between items-center">
+          <div className="text-4xl bg-gray-300 p-2 px-4 rounded-2xl ">
+            LogoHolder
           </div>
-          <div className="relative pb-1 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-500 hover:after:w-full">
-            Adatkezelési tájékoztató
+          <div
+            className="flex flex-col ml-4
+        "
+          >
+            <div className="text-2xl font-bold mb-7">Címünk</div>
+            <div className="">1119 Random cím 12-14</div>
           </div>
-        </motion.div>
 
-        <motion.div
-          variants={footerRightSideVariants}
-          className="flex justify-center items-center cursor-pointer "
-        >
-          <motion.div
-            whileHover={{ scale: 1.1 }}
-            transition={{ type: "spring", bounce: 0.7 }}
+          <div
+            className="flex flex-col ml-4
+        "
           >
-            <SocialMediaIcon name="insta" />
+            <div className="text-2xl font-bold mb-7">Nyitvatartás</div>
+            <div className="">Hétfőn zárva</div>
+            <div className="">K-P 12:00-19:00</div>
+            <div className="">Szombat 12:00-18:00</div>
+            <div className="">Vasárnap 13:00-17:00</div>
+          </div>
+
+          <motion.div
+            variants={footerRightSideVariants}
+            className="flex flex-col cursor-pointer ml-4 space-y-2 group"
+          >
+            <div className="relative pb-1 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-500 hover:after:w-full">
+              Felelősséválalási nyilatkozat
+            </div>
+            <div className="relative pb-1 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[2px] after:bg-blue-500 after:transition-all after:duration-500 hover:after:w-full">
+              Adatkezelési tájékoztató
+            </div>
           </motion.div>
 
           <motion.div
-            whileHover={{ scale: 1.1 }}
-            transition={{ type: "spring", bounce: 0.7 }}
+            variants={footerRightSideVariants}
+            className="flex justify-center items-center cursor-pointer "
           >
-            <SocialMediaIcon name="fb" />
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", bounce: 0.7 }}
+            >
+              <SocialMediaIcon name="insta" />
+            </motion.div>
+
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", bounce: 0.7 }}
+            >
+              <SocialMediaIcon name="fb" />
+            </motion.div>
+            <motion.div
+              whileHover={{ scale: 1.1 }}
+              transition={{ type: "spring", bounce: 0.7 }}
+            >
+              <SocialMediaIcon name="tiktok" />
+            </motion.div>
           </motion.div>
-          <motion.div
-            whileHover={{ scale: 1.1 }}
-            transition={{ type: "spring", bounce: 0.7 }}
-          >
-            <SocialMediaIcon name="tiktok" />
-          </motion.div>
-        </motion.div>
+        </div>
       </motion.div>
     </>
   );
