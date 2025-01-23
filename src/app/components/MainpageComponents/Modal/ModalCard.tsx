d40fdfa0751eb5f0857febd87902a863acd1fa73
molnar.david <molnar.david@wigner.hu>
2025-01-23 23:30:26 +0100

    card design update

--- src/app/components/MainpageComponents/Modal/ModalCard.tsx
+++ src/app/components/MainpageComponents/Modal/ModalCard.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import { motion } from "framer-motion";
+
 type ModalCardProps = {
   CardPicture: string;
   CardText: string;
@@ -20,24 +21,21 @@ const prices = [
     price: "10000 Ft",
   },
 ];
-const transition = {
-  duration: 0.8,
-  delay: 0.5,
-  ease: [0, 0.71, 0.2, 1.01],
-};
 
 const ModalCard: React.FC<ModalCardProps> = ({ CardPicture, CardText }) => {
   const basePrice = "5000 Ft";
   const [price, setPrice] = useState<string>(basePrice);
+  const [selectedPacking, setSelectedPacking] = useState<string>("50 g"); // State to track selected packing
 
-  const handleSetPrice = (item: any) => {
+  const handleSetPrice = (item: { packing: string; price: string }) => {
     setPrice(item.price);
+    setSelectedPacking(item.packing); // Update selected packing state
   };
 
   return (
     <>
-      <div className="w-[500px]   absolute flex justify-center flex-col items-center">
-        <div className=" rounded-t-3xl w-full h-[150px] flex justify-center items-center">
+      <div className="w-[450px] absolute flex justify-center flex-col items-center">
+        <div className="rounded-t-3xl w-full h-[150px] flex justify-center items-center">
           <Image
             alt="card picture"
             src="/images/HeroPicture.jpg"
@@ -46,30 +44,32 @@ const ModalCard: React.FC<ModalCardProps> = ({ CardPicture, CardText }) => {
             className="rounded-t-3xl"
           />
         </div>
-        <div className="bg-white rounded-b-3xl w-full Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut volutpat ligula. Vivamus fringilla, dolor ut accumsan cursus, sapien ipsum ultricies nibh, ut malesuada velit nulla vel risus. Donec quis feugiat ante, ut maximus neque. Curabitur vitae pulvinar sapien. Etiam lobortis sollicitudin lacus interdum placerat. Quisque ante libero, dictum et eros ac, feugiat faucibus urna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Integer mi magna, lacinia in tortor vel, varius malesuada ante. Phasellus pretium congue massa. Curabitur nec orci eu erat lobortis aliquam at et lorem">
-          <div className="ml-8 mt-5  text-2xl">{CardText}</div>
-          <div className="p-4">
+        <div className="bg-white rounded-b-3xl w-full p-4">
+          <div className="my-5 text-4xl">{CardText}</div>
+          <div className="text-gray-500">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut
             volutpat ligula. Vivamus fringilla, dolor ut accumsan cursus, sapien
             ipsum ultricies nibh, ut malesuada velit nulla vel risus. Donec quis
             feugiat ante, ut maximus neque. Curabitur vitae pulvinar sapien.
-            Etiam lobortis sollicitudin lacus interngilla, dolor ut accumsan
-            cursus, sapien ipsum ultricies nibh, ut malesuada velit nulla vel
-            risus. Donec quis feugiat ante, ut maximus neque. Curabitur vitae
-            pulvinar sapien. Etiam lobortis sollicitudin lacus interdum
-            placerat.
+            Etiam lobortis sollicitudin lacus interngilla.
           </div>
-          <div className="">Kiszerelés</div>
-          <div className="flex  items-center  text-center justify-between ">
-            {prices.map((item) => (
-              <div
-                onClick={() => handleSetPrice(item)}
-                key={item.packing}
-                className="hover:bg-blue-600 border-2 rounded-3xl w-14 hover:rounded-3xl p-1 cursor-pointer mx-4 "
-              >
-                {item.packing}
-              </div>
-            ))}
+          <div className="border-t-2 pt-5 text-xl mt-5">Kiszerelés</div>
+          <div className="flex justify-center">
+            <div className="flex mt-5 items-center text-center justify-between">
+              {prices.map((item) => (
+                <div
+                  onClick={() => handleSetPrice(item)}
+                  key={item.packing}
+                  className={`w-[7rem] rounded-3xl hover:bg-green-200 p-1 cursor-pointer mx-4 ${
+                    selectedPacking === item.packing
+                      ? "bg-green-200"
+                      : " bg-green-100" // Default background for unselected items
+                  }`}
+                >
+                  {item.packing}
+                </div>
+              ))}
+            </div>
           </div>
           <motion.div
             key={price}
@@ -80,9 +80,9 @@ const ModalCard: React.FC<ModalCardProps> = ({ CardPicture, CardText }) => {
               delay: 0.2,
               ease: [0, 0.71, 0.2, 1.01],
             }}
-            className="flex justify-center mt-6 "
+            className="flex justify-center mt-6"
           >
-            <div className="bg-green-500 mb-3 p-2 rounded-3xl text-white  ">
+            <div className="bg-green-500 mb-3 p-2 rounded-3xl text-white">
               {price}
             </div>
           </motion.div>
