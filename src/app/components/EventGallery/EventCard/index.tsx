1f05e0d3e5f728d1c4295a5341bb9e7bdcdfff38
molnar.david <molnar.david@wigner.hu>
2025-01-24 13:58:28 +0100

    basic imports setup

---
+++ src/app/components/EventGallery/EventCard/index.tsx
@@ -0,0 +1,110 @@
+import * as React from "react";
+import { memo, useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+
+import { useInvertedBorderRadius } from "../utils/use-inverted-border-radios";
+import Link from "next/link";
+import { CardData } from "../types";
+import { ContentPlaceholder } from "./ContentPlaceholder";
+import { Title } from "./Title";
+import { Image } from "./Image";
+import { openSpring, closeSpring } from "./animations";
+import { useScrollConstraints } from "../utils/use-scroll-constrains";
+import { useWheelScroll } from "../utils/use-wheel-scrolls";
+
+interface Props extends CardData {
+  isSelected?: boolean;
+  history?: {
+    push: (route: string) => void;
+  };
+}
+
+// Distance in pixels a user has to scroll a card down before we recognise
+// a swipe-to dismiss action.
+const dismissDistance = 150;
+
+export const Card = memo(
+  ({
+    isSelected,
+    id,
+    title,
+    category,
+    history,
+    pointOfInterest,
+    backgroundColor,
+  }: Props) => {
+    const y = useMotionValue(0);
+    const zIndex = useMotionValue(isSelected ? 2 : 0);
+
+    // Maintain the visual border radius when we perform the layoutTransition by inverting its scaleX/Y
+    const inverted = useInvertedBorderRadius(20);
+
+    // We'll use the opened card element to calculate the scroll constraints
+    const cardRef = useRef(null);
+    const constraints = useScrollConstraints(cardRef, isSelected!);
+
+    function checkSwipeToDismiss() {
+      y.get() > dismissDistance && history!.push("/");
+    }
+
+    function checkZIndex(latest: any) {
+      if (isSelected) {
+        zIndex.set(2);
+      } else if (!isSelected && latest.scaleX < 1.01) {
+        zIndex.set(0);
+      }
+    }
+
+    // When this card is selected, attach a wheel event listener
+    const containerRef = useRef<HTMLLIElement>(null!);
+    useWheelScroll(
+      containerRef,
+      y,
+      constraints,
+      checkSwipeToDismiss,
+      isSelected!
+    );
+
+    return (
+      <li ref={containerRef} className={`card`}>
+        <Overlay isSelected={isSelected!} />
+        <div className={`card-content-container ${isSelected && "open"}`}>
+          <motion.div
+            ref={cardRef}
+            className="card-content"
+            style={{ ...inverted, zIndex, y }}
+            layout
+            transition={isSelected ? openSpring : closeSpring}
+            drag={isSelected ? "y" : false}
+            dragConstraints={constraints}
+            onDrag={checkSwipeToDismiss}
+            onUpdate={checkZIndex}
+          >
+            <Image
+              id={id}
+              isSelected={isSelected!}
+              pointOfInterest={pointOfInterest}
+              backgroundColor={backgroundColor}
+            />
+            <Title title={title} category={category} isSelected={isSelected!} />
+            <ContentPlaceholder />
+          </motion.div>
+        </div>
+        {!isSelected && <Link href={id} className={`card-open-link`} />}
+      </li>
+    );
+  },
+  (prev, next) => prev.isSelected === next.isSelected
+);
+
+const Overlay = ({ isSelected }: { isSelected: boolean }) => (
+  <motion.div
+    initial={false}
+    animate={{ opacity: isSelected ? 1 : 0 }}
+    transition={{ duration: 0.2 }}
+    style={{ pointerEvents: isSelected ? "auto" : "none" }}
+    className="overlay"
+  >
+    <Link href="/" />
+  </motion.div>
+);
---
+++ src/app/components/EventGallery/Header.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { Avatar } from "react-lorem-ipsum";
+
+export const Header = () => (
+  <header>
+    <span className="text-[14px] ">Tuesday, August 13th</span>
+    <h1>Today</h1>
+  </header>
+);
